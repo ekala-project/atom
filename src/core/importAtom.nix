713a3adffa94e7d64b209b9073ba2fa73080bcb3
Timothy DeHerrera <tim@nrdxp.dev>
2024-12-16 20:16:48 -0700

    feat(lock): import atoms from published refs
    The evaluation will prefer a local path, if it exists, to make
    interactive development in repos with more than one atom convenient.

--- src/core/importAtom.nix
+++ src/core/importAtom.nix
@@ -96,13 +96,25 @@ let
       if builtins.pathExists lockPath && lock.version == 1 then
         builtins.listToAttrs (
           map (dep: {
-            inherit (dep) name;
+            name = dep.name or dep.id;
             value =
               let
-                path = "${root}/${dep.path}@.toml";
+                path = "${root}/${dep.path or dep.id}@.toml";
               in
-              if dep ? version && dep ? path && builtins.pathExists path then
-                (import ./importAtom.nix) { } path
+              if dep ? version && dep ? id then
+                if builtins.pathExists path then
+                  (import ./importAtom.nix) { } path
+                else
+                  let
+                    spec = baseNameOf path;
+                  in
+                  (import ./importAtom.nix) { }
+                    "${
+                      (builtins.fetchGit {
+                        inherit (dep) url rev;
+                        ref = "refs/atoms/${dep.id}/${dep.version}/atom";
+                      })
+                    }/${spec}"
               else if dep ? rev then
                 let
                   repo = builtins.fetchGit {
