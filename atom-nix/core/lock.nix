b7f9f3e70e5fc4299c9ffcb48f75105f28767d75
Timothy DeHerrera <tim@nrdxp.dev>
2025-07-20 20:02:54 -0600

    feat(lock): add pin pulling from other atoms
    Enable pins to source from another atom using a `from` key, avoiding
    duplicate specs and manual syncing. Support overriding the `path` key
    as well as using the `get` key to set a different `name` than the
    downstream pin. Otherwise, it will cook for a pin of the same `name`.

--- atom-nix/core/lock.nix
+++ atom-nix/core/lock.nix
@@ -2,67 +2,96 @@ root: src: url:
 let
   lockPath = "${root}/${src}.lock";
   lock = builtins.fromTOML (builtins.readFile lockPath);
+  importAtom = import ./importAtom.nix { };
+  atomPath =
+    dep:
+    let
+      path = "${root}/${dep.path or dep.id}@.toml";
+    in
+    if builtins.pathExists path then
+      path
+    else
+      let
+        manifest = baseNameOf path;
+      in
+      "${
+        (fetchGit {
+          inherit (dep) rev;
+          inherit url;
+          ref = "refs/eka/atoms/${dep.id}/${dep.version}";
+        })
+      }/${manifest}";
+
+  depsToSet =
+    list:
+    builtins.listToAttrs (
+      map (dep: {
+        name = dep.name or dep.id;
+        value = dep;
+      }) list
+    );
+
+  deps = depsToSet lock.deps or [ ];
+
+  importPin =
+    dep: fetch:
+    let
+      fromDeps = depsToSet fromLock.deps or [ ];
+      fromAtom = deps.${dep.from};
+      fromPath = atomPath fromAtom;
+      fromLockPath = "${(dirOf fromPath)}/${fromAtom.id}.lock";
+      fromLock = builtins.fromTOML (builtins.readFile fromLockPath);
+      fromName = dep.get or dep.name or "";
+      fromDep = fromDeps.${fromName};
+    in
+    if
+      dep ? from
+      && fromAtom.type or "" == "atom"
+      && builtins.pathExists fromLockPath
+      && fromDeps ? ${fromName}
+      && builtins.match "^pin.*" fromDep.type != null
+    then
+      builtins.traceVerbose "using a pin `${fromName}` as `${dep.name}` from atom `${dep.from}` in `${src}`" (
+        importDep (fromDep // { path = dep.path or fromDep.path or "."; })
+      )
+    else
+      import (if dep ? path then "${fetch}/${dep.path}" else fetch);
+
+  importDep =
+    dep:
+    if dep.type == "atom" then
+      importAtom (atomPath dep)
+    else if dep.type == "pin+git" then
+      let
+        repo = fetchGit {
+          inherit (dep) rev url;
+          shallow = true;
+        };
+      in
+      importPin dep repo
+    else if dep.type == "pin+tar" then
+      let
+        fetch = fetchTarball {
+          inherit (dep) url;
+          sha256 = dep.checksum;
+        };
+      in
+      importPin dep fetch
+    else if dep.type == "pin" then
+      let
+        fetch = builtins.fetchurl {
+          inherit (dep) url;
+          sha256 = dep.checksum;
+        };
+      in
+      importPin dep fetch
+    else
+      { };
+
 in
 if builtins.pathExists lockPath && lock.version == 1 then
   let
-    from = builtins.listToAttrs (
-      map (dep: {
-        name = dep.name or dep.id;
-        value =
-          let
-            path = "${root}/${dep.path or dep.id}@.toml";
-          in
-          if dep.type == "atom" then
-            if builtins.pathExists path then
-              (import ./importAtom.nix) { } path
-            else
-              let
-                manifest = baseNameOf path;
-              in
-              (import ./importAtom.nix) { }
-                "${
-                  (fetchGit {
-                    inherit (dep) rev;
-                    inherit url;
-                    ref = "refs/eka/atoms/${dep.id}/${dep.version}";
-                  })
-                }/${manifest}"
-          else if dep.type == "pin+git" then
-            let
-              repo = fetchGit {
-                inherit (dep) rev url;
-                shallow = true;
-              };
-            in
-            import (if dep ? path then "${repo}/${dep.path}" else repo)
-          else if dep.type == "pin+tar" then
-            let
-              fetch = fetchTarball {
-                inherit (dep) url;
-                sha256 = dep.checksum;
-              };
-            in
-            import (if dep ? path then "${fetch}/${dep.path}" else fetch)
-          else if dep.type == "pin" then
-            let
-              fetch = builtins.fetchurl {
-                inherit (dep) url;
-                sha256 = dep.checksum;
-              };
-            in
-            import (if dep ? path then "${fetch}/${dep.path}" else fetch)
-          else if dep.type == "src" then
-            let
-              src = import <nix/fetchurl.nix> {
-                inherit (dep) url;
-                hash = dep.checksum;
-              };
-            in
-            src
-          else
-            { };
-      }) lock.deps or [ ]
-    );
+    from = builtins.mapAttrs (_: dep: importDep dep) deps;
     get = builtins.listToAttrs (
       map (src: {
         name = src.pname or src.name;
